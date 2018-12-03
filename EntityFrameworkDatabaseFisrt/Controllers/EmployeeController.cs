5b91afef806a6d53a8288f5ab4c934417eb27c13
longdnd00573 <longdnd00573@fpt.edu.vn>
2018-12-03 16:32:31 +0700

    DatabaseFirst

---
+++ EntityFrameworkDatabaseFisrt/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityFrameworkDatabaseFisrt.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeContext db = new EmployeeContext();
+        public ActionResult Index()
+        {
+            return View(db.Employees.ToList());
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+    }
+}
---
+++ EntityFrameworkDatabaseFisrt/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EntityFrameworkDatabaseFisrt.MvcApplication" Language="C#" %>
